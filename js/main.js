5eb0f8f398fa634a02081659f963aff617d3752d
aliaksandrow <aliaksandrou@gmail.com>
2016-11-21 12:01:19 +0300

    Shopno

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/* sticky */
+   $(window).on('scroll', function() {
+		var y = $(window).scrollTop(),
+		    topBar = $('header');    
+	   if (y > 1) {
+	      topBar.addClass('sticky');
+	   }
+      else {
+         topBar.removeClass('sticky');
+      } 
+	});	
+      /* section-servises hover */  
+$(document).ready(function(){
+    $('.design div').hover(function(){
+$('.hover',this).show();
+},
+function(){
+$('.hover',this).hide();
+});
+})
+      /* section-team hover */
+$(document).ready(function(){
+    $('.photo-wall div').hover(function(){
+$('.hover',this).show();
+},
+function(){
+$('.hover',this).hide();
+});
+}) 
+/*
+$(document).ready(function() {
+	$('.design div').hover(
+		function(){$('.hover',this).fadeIn('fast');},
+		function(){$('.hover',this).fadeOut('fast');
+     });
+});*/
